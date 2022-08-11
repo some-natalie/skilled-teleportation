492053602cb58ea0ede7754516903da4f3ae3c1d
Natalie Somersall <some-natalie@github.com>
2022-08-11 09:19:26 -0600

    removing that trailing slash really took too long

--- scripts/teleport-push.sh
+++ scripts/teleport-push.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# This script is used to sync a bunch of GitHub Actions from your local machine 
+# This script is used to sync a bunch of GitHub Actions from your local machine
 # to GHES, taking the extracted ZIP archive from the Actions run or extracted
 # tarball from `teleport-pull.sh`.
 
@@ -9,16 +9,30 @@
 # $2: The GHES token to use (e.g. "ghp_123456789")
 
 # For each directory in the archive, sync the Actions to GHES
+echo "Syncing Actions to GHES..."
 actions-sync push --cache-dir "./cache" --destination-url "$1" --destination-token "$2"
 
+# Set the headers for curl
+HEADER="Authorization: token $2"
+
+# Remove the trailing slash from the server URL if it exists
+if [[ "$1" == *"/" ]]; then
+    URL="${1::-1}"
+else
+    URL="$1"
+fi
+
 # Make the skills repositories templates
-curl -s "$1"/api/v3/orgs/skills/repos --header "Authorization: token $2" | jq -r '.[].full_name' | while read -r line ; do
-    curl -sS -X PATCH "$1"/api/v3/repos/"$line" --header "Authorization: token $2" --data '{"is_template":true}'
+echo "Making the skills repositories templates..."
+curl -s --header "$HEADER" "$URL"/api/v3/orgs/skills/repos | jq -r '.[].full_name' | while read -r line ; do
+   curl -sS -o /dev/null --show-error --fail -X PATCH --header "$HEADER" "$URL"/api/v3/repos/"$line" --data '{"is_template":true}'
 done
 
 # Make the starter Actions templates
+echo "Making the starter Actions templates..."
 starters=("typescript-action" "javascript-action" "hello-world-docker-action" "hello-world-javascript-action" "container-action" "container-toolkit-action")
 
 for i in "${starters[@]}" ; do
-    curl -sS -X PATCH "$1"/api/v3/repos/actions/"$i" --header "Authorization: token $2" --data '{"is_template":true}' || true
+    curl -sS -o /dev/null --show-error --fail -X PATCH --header "$HEADER" "$URL"/api/v3/repos/actions/"$i" --data '{"is_template":true}' || true
 done
+
